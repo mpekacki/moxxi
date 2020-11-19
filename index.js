86e264c0cee9c5da3cb40d67e980c98f3fa8b481
Marcel Pekacki <marcel.x.pekacki@gsk.com>
2020-11-19 16:18:49 +0100

    initial commit (prototype)

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const PORT = process.env.PORT || 5000;
+// lib/app.ts
+var express = require("express");
+var WebSocket = require("ws");
+// Create a new express application instance
+var app = express();
+var wss = new WebSocket.Server({ port: 8080 });
+wss.on('connection', function (ws) {
+    var serverId = Math.random().toString(36).substring(7);
+    var requests = {};
+    var lastRequestKey = 0;
+    var endpointData = { serverId: serverId };
+    ws.send(JSON.stringify(endpointData));
+    ws.on('message', function (message) {
+        var incomingResponse = JSON.parse(message);
+        if (incomingResponse.requestKey in requests) {
+            var res = requests[incomingResponse.requestKey];
+            if (!res.headersSent) {
+                res.status(incomingResponse.statusCode).json(JSON.parse(incomingResponse.json));
+            }
+        }
+    });
+    app.all('/' + serverId + '*', function (req, res) {
+        requests[++lastRequestKey] = res;
+        var requestData = { serverId: serverId, requestKey: lastRequestKey, method: req.method, url: req.url, headers: req.headers, params: req.params, body: req.body };
+        console.log(requestData);
+        ws.send(JSON.stringify(requestData));
+    });
+});
+app.use('/', express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.listen(PORT, function () {
+    console.log(`Example app listening on port ${ PORT }!`);
+});
