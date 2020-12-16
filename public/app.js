5cd18c9ae38b5d6ff3befb5e4dad18f777bb32f8
Marcel Pekacki <marcel.x.pekacki@gsk.com>
2020-12-16 14:21:47 +0100

    fix

--- public/app.js
+++ public/app.js
@@ -79,14 +79,14 @@ const app = new Vue({
                     ]
                 },
                 'milligram': {
-                    name: 'milligram', label: 'MVP', stylesheets: [
+                    name: 'milligram', label: 'Milligram', stylesheets: [
                         'https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic',
                         'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css',
                         'https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css'
                     ]
                 },
                 'mvp': {
-                    name: 'mvp', label: 'Milligram', stylesheets: [
+                    name: 'mvp', label: 'MVP', stylesheets: [
                         'https://unpkg.com/mvp.css'
                     ]
                 },
